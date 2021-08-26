24e56855d31ae2f0326b483a0f41b00695101584
Raffael Martins <devrmartins@gmail.com>
2021-08-26 09:03:14 -0300

    Instalar cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (request: Request, response: Response) => {
     return response.json({ message: 'Hello World!' });
+});
+
+app.post('/', (request: Request, response: Response) => {
+    const { name } = request.body;
+    return response.json({ name });
 });
 
 app.listen(3333, () => {
